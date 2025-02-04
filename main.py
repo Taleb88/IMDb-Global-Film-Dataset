a7c00333d581160624e59a5f59beae48ff8c15f6
Taleb88 <tdamaree18@gmail.com>
2025-02-03 23:37:50 -0500

    performing data cleanup

--- main.py
+++ main.py
@@ -19,8 +19,7 @@ print('endYear:\n',gfg_df['endYear'].unique(),'\n')
 print('runtimeMinutes:\n',gfg_df['runtimeMinutes'].unique(),'\n')
 print('genres:\n',gfg_df['genres'].unique(),'\n')
 
-gfg_df['isAdult'] = gfg_df['isAdult'].replace("\\N",0)
-gfg_df['isAdult'] = gfg_df['isAdult'].astype(int)
+gfg_df['isAdult'] = gfg_df['isAdult'].replace("\\N",0).astype(int)
 
 def isAdult(x):
     try:
@@ -33,12 +32,33 @@ def isAdult(x):
 
 gfg_df['isAdult'] = gfg_df.apply(lambda x: isAdult(x.iloc[4]), axis='columns')
 
+print('\nisAdult:',gfg_df['isAdult'].unique(),'\n')
+
 gfg_df['startYear'] = gfg_df['startYear'].replace("\\N",0000).astype(int)
 gfg_df['endYear'] = gfg_df['endYear'].replace("\\N",0000).astype(int)
 
-print('\nisAdult:',gfg_df['isAdult'].unique(),'\n')
 print('\nstartYear:',gfg_df['startYear'].unique(),'\n')
 print('\nendYear:',gfg_df['endYear'].unique(),'\n')
+
+runtime_minutes_errors = ['Reality-TV','Talk-Show','Documentary','Family,Game-Show','Animation,Comedy,Family',
+         'News,Talk-Show','Comedy,News,Talk-Show','Documentary,Reality-TV','Comedy,Drama,Fantasy',
+         'Fantasy,Horror,Mystery','Action,Fantasy,Horror','Action,Horror,Mystery','Comedy,Drama,Horror',
+         'Action,Adventure,Drama','Drama,Fantasy,Horror','Game-Show,Reality-TV']
+
+def runtimeMinutes(x):
+    try:
+        if x == runtime_minutes_errors:
+            return '0'
+        else:
+            return f'{x}'
+    except Exception as e:
+        return f'error - cannot make proposed changes {type(e)}'
+    
+gfg_df['runtimeMinutes'] = gfg_df.apply(lambda x: runtimeMinutes(x.iloc[7]), axis='columns')
+
+gfg_df['runtimeMinutes'] = gfg_df['runtimeMinutes'].replace("\\N",'0').astype(str)
+
+print('runTimeMinutes updated accordingly:\n',gfg_df)
 
 def movie_1980s(df):
     try:
