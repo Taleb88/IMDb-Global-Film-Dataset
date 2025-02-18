0fbfb1dec89a78f5c7bb0702552cc052dca11934
Taleb88 <tdamaree18@gmail.com>
2025-02-18 11:41:03 -0500

    multiple if statements within while loop in query.py; attempting to figure out how to implement for loop to shorten code in query.py

--- query.py
+++ query.py
@@ -14,19 +14,50 @@ gfg_tv_special_df = pd.read_csv('gfg_tv_special.csv')
 gfg_video_df = pd.read_csv('gfg_video.csv')
 gfg_videogame_df = pd.read_csv('gfg_videogame.csv')
 
-array = ['gfg_ratings_merged_df']
-
 # gfg_movie_df
 while True:
     df_answer = input('\ntype in name of dataframe:\n')
-    # print(f"{gfg_movie_df.loc[(gfg_movie_df['primaryTitle'].str.contains(answer, na=False))]}")
-    
-    for x in array:
-        if df_answer == f'{x}':
-            primary_title_answer = input('\nenter primaryTitle value\n:')
-            print(f"{gfg_movie_df.loc[(gfg_movie_df['primaryTitle'].str.contains(primary_title_answer, na=False))]}")
-            
-            
+
+    if df_answer == 'gfg_ratings_merged_df':
+        primary_title_answer = input('\nenter primaryTitle value\n:')
+        print(f"{gfg_ratings_merged_df.loc[(gfg_ratings_merged_df['primaryTitle'].str.contains(primary_title_answer, na=False))]}")    
+    if df_answer == 'fav_tv_shows_df':
+        primary_title_answer = input('\nenter primaryTitle value\n:')
+        print(f"{fav_tv_shows_df.loc[(fav_tv_shows_df['primaryTitle'].str.contains(primary_title_answer, na=False))]}")
+    if df_answer == 'gfg_movie_df':
+        primary_title_answer = input('\nenter primaryTitle value\n:')
+        print(f"{gfg_movie_df.loc[(gfg_movie_df['primaryTitle'].str.contains(primary_title_answer, na=False))]}")    
+    if df_answer == 'gfg_short_df':
+        primary_title_answer = input('\nenter primaryTitle value\n:')
+        print(f"{gfg_short_df.loc[(gfg_short_df['primaryTitle'].str.contains(primary_title_answer, na=False))]}")
+    if df_answer == 'gfg_tv_episode_df':
+        primary_title_answer = input('\nenter primaryTitle value\n:')
+        print(f"{gfg_tv_episode_df.loc[(gfg_tv_episode_df['primaryTitle'].str.contains(primary_title_answer, na=False))]}")    
+    if df_answer == 'gfg_tv_miniseries_df':
+        primary_title_answer = input('\nenter primaryTitle value\n:')
+        print(f"{gfg_tv_miniseries_df.loc[(gfg_tv_miniseries_df['primaryTitle'].str.contains(primary_title_answer, na=False))]}")
+    if df_answer == 'gfg_tv_movie_df':
+        primary_title_answer = input('\nenter primaryTitle value\n:')
+        print(f"{gfg_tv_movie_df.loc[(gfg_tv_movie_df['primaryTitle'].str.contains(primary_title_answer, na=False))]}")
+    if df_answer == 'gfg_tv_pilot_df':
+        primary_title_answer = input('\nenter primaryTitle value\n:')
+        print(f"{gfg_tv_pilot_df.loc[(gfg_tv_pilot_df['primaryTitle'].str.contains(primary_title_answer, na=False))]}")
+    if df_answer == 'gfg_tv_series_df':
+        primary_title_answer = input('\nenter primaryTitle value\n:')
+        print(f"{gfg_tv_series_df.loc[(gfg_tv_series_df['primaryTitle'].str.contains(primary_title_answer, na=False))]}")
+    if df_answer == 'gfg_tv_short_df':
+        primary_title_answer = input('\nenter primaryTitle value\n:')
+        print(f"{gfg_tv_short_df.loc[(gfg_tv_short_df['primaryTitle'].str.contains(primary_title_answer, na=False))]}")
+    if df_answer == 'gfg_tv_special_df':
+        primary_title_answer = input('\nenter primaryTitle value\n:')
+        print(f"{gfg_tv_special_df.loc[(gfg_tv_special_df['primaryTitle'].str.contains(primary_title_answer, na=False))]}")
+    if df_answer == 'gfg_video_df':
+        primary_title_answer = input('\nenter primaryTitle value\n:')
+        print(f"{gfg_video_df.loc[(gfg_video_df['primaryTitle'].str.contains(primary_title_answer, na=False))]}")
+    if df_answer == 'gfg_videogame_df':
+        primary_title_answer = input('\nenter primaryTitle value\n:')
+        print(f"{gfg_videogame_df.loc[(gfg_videogame_df['primaryTitle'].str.contains(primary_title_answer, na=False))]}")
 
     if df_answer == 'exit query':
         break
+                     
