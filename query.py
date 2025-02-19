aadd7d1ee4be2674b1c0a97df170ede567e60e6c
Taleb88 <tdamaree18@gmail.com>
2025-02-19 18:43:43 -0500

    code refactored in query.py

--- query.py
+++ query.py
@@ -2,13 +2,45 @@ import pandas as pd
 
 dataframes = ['gfg_ratings_merged','fav_tv_shows','gfg_movie','gfg_short','gfg_tv_episode','gfg_tv_miniseries',
          'gfg_tv_movie','gfg_tv_series','gfg_tv_short','gfg_tv_special','gfg_video','gfg_videogame']
+# capture primary title value only
 while True:
-    df_answer = input('\ntype in name of dataframe:\n')
+    try: 
+        df_answer = input('\ntype in name of dataframe:\n')
 
-    for df_name in dataframes:
         if df_answer == 'exit query':
             break
-        if df_answer in df_name:
-            df = pd.read_csv(f'{df_name}.csv')
-            primary_title_answer = input('primary title:\n')
-            print(f"{df[(df['primaryTitle'].str.contains(primary_title_answer, na=False))]}")
+
+        for df_name in dataframes:
+            if df_answer == 'exit query':
+                break
+            if df_answer in df_name:
+                df = pd.read_csv(f'{df_name}.csv')
+                primary_title_values = list(df['primaryTitle'].values)
+                primary_title_answer = input('primary title:\n')
+                if (primary_title_answer in primary_title_values):
+                    print(f"{df[(df['primaryTitle'].str.contains(primary_title_answer, na=False))]}")                                  
+    except Exception as e:
+        print(f'error - dataframe does not exist - {type(e)}')
+        continue
+# capture primary title + start year values
+while True:
+    try: 
+        df_answer = input('\ntype in name of dataframe:\n')
+        
+        if df_answer == 'exit query':
+            break
+        
+        for df_name in dataframes:
+            if df_answer == 'exit query':
+                break            
+            if df_answer in df_name:
+                df = pd.read_csv(f'{df_name}.csv')
+                primary_title_values = list(df['primaryTitle'].values)
+                start_year_values = list(df['startYear'].values)
+                primary_title_answer = input('primary title:\n')
+                start_year_answer = int(input('start year:\n'))
+                if (primary_title_answer in primary_title_values) and (start_year_answer in start_year_values):
+                    print(f"{df[(df['primaryTitle'].str.contains(primary_title_answer, na=False)) & (df['startYear'] == start_year_answer)]}")    
+    except Exception as e:
+        print(f'error - dataframe does not exist - {type(e)}')
+        continue                    
