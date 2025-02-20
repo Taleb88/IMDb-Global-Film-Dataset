33af00e5c31ef707693668ad66f630338bfc6180
Taleb88 <tdamaree18@gmail.com>
2025-02-19 19:12:06 -0500

    code refactoring in main.py - in progress

--- main.py
+++ main.py
@@ -126,9 +126,9 @@ gfg_ratings_merged_df.to_csv('gfg_ratings_merged.csv', index=False)
 print('gfg_ratings_merged_df:\n',gfg_ratings_merged_df.head(50),'\n')
 
 # VALUE CORRECTIONS - MISCELLANEOUS - IN PROGRESS
-gfg_ratings_merged_df.loc[gfg_df['primaryTitle'] == 'My Wife and Kids', 'startYear'] = 2001
-gfg_ratings_merged_df.loc[gfg_df['primaryTitle'] == 'New york Undercover', 'endYear'] = 1998
-gfg_ratings_merged_df.loc[gfg_df['primaryTitle'] == 'New york Undercover', 'endYear'] = 1993
+gfg_ratings_merged_df.loc[gfg_ratings_merged_df['primaryTitle'] == 'My Wife and Kids', 'startYear'] = 2001
+gfg_ratings_merged_df.loc[gfg_ratings_merged_df['primaryTitle'] == 'New york Undercover', 'endYear'] = 1998
+gfg_ratings_merged_df.loc[gfg_ratings_merged_df['primaryTitle'] == 'New york Undercover', 'endYear'] = 1993
 gfg_ratings_merged_df.to_csv('gfg_ratings_merged.csv', index=False)
 
 # top 50 tv series; min of 50,000 votes
